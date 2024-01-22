d8239c0b36f9b0b4ab8d1f4bc4946666da64f5d0
Carlos Ramirez <karlossander92@gmail.com>
2024-01-22 16:43:27 -0500

    Primer Dia

--- Day 1 - Beginner - Working with Variables in Python to Manage Data/01 Printing to the console in pyton.py
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/01 Printing to the console in pyton.py
@@ -1,3 +1,17 @@
 # Escribe codigo en la parte de abajo, esto es un comentario
 # Funcion de impimir en concola
-print("Hola Mundo!!")
+print("Hola Mundo!! en una linea")
+
+## Otra forma es hacer un salto de linea
+print("Hola mundo\nDesde Python")
+
+# Concatenando
+print("Dos frases" + " Desde Python")
+
+# Vamos a ingresar datosa
+print("Hola " + input("¿ Como es tu nombre ?\n") + "!")
+
+# Variables en python
+nombre = input("Como es tu nombre ? \n")
+edad = input("Cuantos años tienes ? \n")
+print("El nombre es: " + nombre + " y tiene " + edad + " añosa")
---
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/Project 1 Band-name-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+name = input("What's name of the city you grew up in ?\n")
+pets = input("What's yout pet's name ?\n")
+print("You band name could be " + name + " " + pets)
